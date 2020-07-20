5c520444bc678f5308fc2d44eefeebb98ad79dfa
metmirr <metmirr@gmail.com>
2020-07-20 11:37:37 +0300

    add event handler for Proposal:Rejected, Proposal:Awarded

--- mappings/treasury.ts
+++ mappings/treasury.ts
@@ -1,13 +1,46 @@
 import { SubstrateEvent, DB } from '../generated/indexer';
 import { Proposal } from '../generated/graphql-server/src/modules/proposal/proposal.model';
+import { assert } from 'console';
 
+// New proposal
 export async function handleProposed(db: DB, event: SubstrateEvent) {
+  const { ProposalIndex } = event.event_params;
   if (event.extrinsic) {
     const proposal = new Proposal();
+    proposal.proposalIndex = ProposalIndex.toString();
     proposal.value = event.extrinsic?.args[0].toString();
     proposal.bond = event.extrinsic?.args[0].toString();
     proposal.beneficiary = event.extrinsic?.args[1].toString();
     proposal.proposer = event.extrinsic?.signer.toString();
-    db.save<Proposal>(proposal);
+    proposal.rejected = false;
+    proposal.approved = false;
+
+    await db.save<Proposal>(proposal);
+  }
+}
+
+// A proposal was rejected
+export async function handleRejected(db: DB, event: SubstrateEvent) {
+  const { ProposalIndex } = event.event_params;
+  const proposal = await db.get(Proposal, { where: { proposalIndex: ProposalIndex } });
+
+  assert(proposal, 'Proposal not found! Invalid proposal id');
+
+  if (proposal) {
+    proposal.rejected = true;
+    await db.save<Proposal>(proposal);
+  }
+}
+
+// A proposal is approved! Some funds have been allocated.
+export async function handleAwarded(db: DB, event: SubstrateEvent) {
+  const { ProposalIndex } = event.event_params;
+  const proposal = await db.get(Proposal, { where: { proposalIndex: ProposalIndex } });
+
+  assert(proposal, 'Proposal not found! Invalid proposal id');
+
+  if (proposal) {
+    proposal.approved = true;
+    await db.save<Proposal>(proposal);
   }
 }
